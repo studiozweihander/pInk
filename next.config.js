e328c2064bb94e06b604a1b064c0e74cdff740cf
Zweihander <gustavoferreira.txt@gmail.com>
2025-08-14 18:18:03 -0300

    chore: estrutura inicial do projeto com Prisma e Supabase

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: []
+  },
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
