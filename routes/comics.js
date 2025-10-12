2ff97d83ce91d1729d5b4a526ae5c04601feaf96
Zweihander <gustavoferreira.txt@gmail.com>
2025-10-12 16:04:00 -0300

    feat: adicionar utilitários e funcionalidades ao controlador de quadrinhos

--- routes/comics.js
+++ routes/comics.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const comicsController = require('../controllers/comicsController');
+const { ComicsController, utils } = require('../controllers/comicsController');
 
-router.get('/', comicsController.getAllComics);
+router.get('/', ComicsController.getAllComics);
 
-router.get('/:id', comicsController.getComicById);
+router.get('/:id', ComicsController.getComicById);
 
-router.get('/:id/issues', comicsController.getComicIssues);
+router.get('/:id/issues', ComicsController.getComicIssues);
+
+router.post('/utils/slugify', (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({
+      success: false,
+      message: 'Title is required'
+    });
+  }
+
+  const slug = utils.createSlug(title);
+  res.json({
+    success: true,
+    slug: slug
+  });
+});
 
 module.exports = router;
