d47ccfe4ee16906f5465e7ddd7453ed8270aa289
Zweihander <gustavodev@duck.com>
2025-12-30 01:48:18 -0300

    🧹 refactor: atualiza Header e ComicCard para utilizar Link e navegação por rotas dinâmicas

--- src/components/ComicCard.tsx
+++ src/components/ComicCard.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Comic } from "../api";
+import { slugify } from "../utils/slugify";
 
 const PLACEHOLDER_IMAGE =
     "https://placehold.co/300x450/242424/e78fde?text=Imagem+Não+Disponível&font=source-sans-pro";
 
 interface ComicCardProps {
     comic: Comic;
-    onClick: () => void;
 }
 
-const ComicCard: React.FC<ComicCardProps> = ({ comic, onClick }) => {
+const ComicCard: React.FC<ComicCardProps> = ({ comic }) => {
     const metaInfo = [
         `Lançamento: ${comic.year || "N/A"}`,
         `Edições: ${comic.total_issues || 0}`,
@@ -17,8 +18,10 @@ const ComicCard: React.FC<ComicCardProps> = ({ comic, onClick }) => {
         `Editora: ${comic.publisher || "N/A"}`,
     ].join(" | ");
 
+    const slug = slugify(comic.title);
+
     return (
-        <div className="card" onClick={onClick}>
+        <Link to={`/${slug}-${comic.year}`} className="card">
             <div className="card-image">
                 <img
                     src={comic.cover || PLACEHOLDER_IMAGE}
@@ -33,7 +36,7 @@ const ComicCard: React.FC<ComicCardProps> = ({ comic, onClick }) => {
                 <h3 className="card-title">{comic.title}</h3>
                 <div className="card-meta">{metaInfo}</div>
             </div>
-        </div>
+        </Link>
     );
 };
 
