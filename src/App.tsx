8af7eaf4e2aa09d51b8bc55a4230f98f64040745
Zweihander <gustavodev@duck.com>
2026-01-25 11:47:39 -0300

    🧹 refactor: adiciona reset de filtros ao mudar de tela e event listener para fechar o dropdown ao clicar fora

--- src/App.tsx
+++ src/App.tsx
@@ -109,9 +109,12 @@ const App: React.FC = () => {
             setCurrentComic(null);
             setCurrentIssues([]);
             setSearchTerm("");
-            setActiveFilters({ publisher: [], year: [], language: [] });
         }
     }, [location.pathname]);
+
+    useEffect(() => {
+        setActiveFilters({ publisher: [], year: [], language: [] });
+    }, [view]);
 
     const loadComics = async () => {
         setIsLoading(true);
