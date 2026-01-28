c3748aafe3d3d37a41646275b75e22d9135dae74
Zweihander <gustavodev@duck.com>
2026-01-28 13:06:30 -0300

    🧬 chore: unifica constantes usadas no repositório

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const APP_VERSION = "2.3.3";
+export const CONTACT_EMAIL = "comics.pink@gmail.com";
+export const PLACEHOLDER_IMAGE =
+  "https://placehold.co/300x450/242424/e78fde?text=Imagem+Não+Disponível&font=source-sans-pro";
+
+export const SOCIAL_LINKS = {
+  twitter: "https://x.com/pinkcomics",
+} as const;
